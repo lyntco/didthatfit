3eb823f30cca26b19334b6dd46793654847eb267
lyntco <strawww@gmail.com>
2014-06-30 22:24:40 +1000

    Thank god for git reset

---
+++ app/controllers/outfits_controller.rb
@@ -0,0 +1,46 @@
+class OutfitsController < ApplicationController
+  def index
+    @outfits = Outfit.all
+  end
+
+  def create
+    @outfit = Outfit.new outfit_params
+    # raise params.inspect
+    if @outfit.save
+      redirect_to( outfits_path )
+    else
+      render :new
+    end
+  end
+
+  def new
+    # raise params.inspect
+    @outfit = Outfit.new
+  end
+
+  def edit
+    @outfit = Outfit.find params[:id]
+  end
+
+  def show
+    @outfit = Outfit.find params[:id]
+  end
+
+  def update
+    @outfit = Outfit.find params[:id]
+    @outfit.update( outfit_params )
+    redirect_to( outfits_path )
+  end
+
+  def destroy
+    outfit = Outfit.find params[:id]
+    outfit.destroy
+    redirect_to( outfits_path )
+  end
+
+  private
+  def outfit_params
+    params.require(:outfit).permit( :name, :image )
+  end
+
+end
