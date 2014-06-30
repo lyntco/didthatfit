5cf4b3685b2cd319d5e239fb2f32b9f2fa70b314
lyntco <strawww@gmail.com>
2014-06-30 22:34:07 +1000

    DOUBLE TAKE THAT NESTED ROUTES

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root :to => 'brands#index'
-  resources :users do
-    resources :items, :except => [:index]
-    resources :outfits, :except => [:index]
-  end
   resources :categories, :only => [:index,:show] do
     resources :types, :only => [:index,:show]
   end
   resources :brands
-  resources :users, :only => [:index,:show]
+  resources :users, :only => [:index]
   resources :items
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/login' => 'sessions#destroy'
   get '/profile' => 'users#show'
+  resources :users, :path => '/' do
+    resources :items, :except => [:index]
+    resources :outfits, :except => [:index]
+  end
 end
