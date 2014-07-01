335ed9c6f465721dfd5b3572663264fe9b9a558a
lyntco <strawww@gmail.com>
2014-07-01 10:12:28 +1000

    Fix check for user and actually works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def smartnav
     links = ''
     if current_user.present?
-      links += "<li>" link_to('Home', user_path(@current_user.username)) "</li>"
+      links += "<li>" + link_to('Home', user_path(@current_user.username)) + "</li>"
     else
-      links += "<li>" link_to('Home', root_path) "</li>"
+      links += "<li>" + link_to('Home', root_path) + "</li>"
     end
     if @current_user.present? && @current_user.is_admin? # @current_user.try(:is_admin)
       links += "<li>" + link_to('View users', users_path) + "</li>"
