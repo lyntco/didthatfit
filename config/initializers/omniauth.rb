2e401874f43ac7df0a187c99bbb28242748c5666
lyntco <strawww@gmail.com>
2014-07-02 22:24:35 +1000

    Really remove fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger
+# OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1517537931811517', 'bdcda321ffa78f5d3c392e84aa757d5a'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, '1517537931811517', 'bdcda321ffa78f5d3c392e84aa757d5a'
+# end
