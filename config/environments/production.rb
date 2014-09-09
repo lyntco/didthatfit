16bf5db7dfd87e7fcddc84f14e750fa6a33d68a7
lyntco <strawww@gmail.com>
2014-09-09 12:19:25 +1000

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,11 +27,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.precompile = true
+  config.assets.compile = false
+  # config.assets.precompile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
