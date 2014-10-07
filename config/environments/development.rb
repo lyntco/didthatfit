e88dc8272c762c2a3e9492236b1a56a0f54a9a10
lyntco <strawww@gmail.com>
2014-10-07 23:07:33 +1100

    Thats the last time i ever listen to stack overflow

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,16 +8,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000'}
+  # config.action_mailer.default_url_options = { host: 'http://www.email.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_EMAIL'],
-    password:             ENV['GMAIL_PASSWORD'],
+    domain:               'heroku.com' ,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
+    openssl_verify_mode: 'none',
     enable_starttls_auto: true  }
 
   # Do not eager load code on boot.
